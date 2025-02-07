7cdf469a896f08ca6f2ddc68700e47cb714bd546
itsjet26 <127301212+itsjet26@users.noreply.github.com>
2025-02-07 23:44:23 +0800

    Update start_facefusion.sh

--- start_facefusion.sh
+++ start_facefusion.sh
@@ -12,7 +12,7 @@ sleep 60
 GRADIO_URL=$(grep -oP 'Running on public URL: \K(https://.*)' /workspace/facefusion.log | tail -1)
 
 if [[ -n "$GRADIO_URL" ]]; then
-    echo "$GRADIO_URL" > /workspaace/facefusion_url.txt
+    echo "$GRADIO_URL" > /workspace/facefusion_url.txt
     echo "✅ FaceFusion Public URL: $GRADIO_URL"
 else
     echo "Failed to get FaceFusion Gradio URL. Falling back to localhost." > /workspace/facefusion_url.txt
