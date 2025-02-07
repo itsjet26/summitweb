b75b593b0aba46fcdf084dd5e0f582d9916484c3
itsjet26 <127301212+itsjet26@users.noreply.github.com>
2025-02-07 22:46:22 +0800

    Update start_facefusion.sh

--- start_facefusion.sh
+++ start_facefusion.sh
@@ -5,17 +5,17 @@ source ~/miniconda/etc/profile.d/conda.sh
 conda activate facefusion
 
 cd /workspace/facefusion
-python -u facefusion.py run 2>&1 | tee facefusion.log &
+python -u facefusion.py run 2>&1 | tee /workspace/facefusion.log &
 
 sleep 60
 
-GRADIO_URL=$(grep -oP 'Running on public URL: \K(https://.*)' facefusion.log | tail -1)
+GRADIO_URL=$(grep -oP 'Running on public URL: \K(https://.*)' /workspace/facefusion.log | tail -1)
 
 if [[ -n "$GRADIO_URL" ]]; then
-    echo "$GRADIO_URL" > facefusion_url.txt
+    echo "$GRADIO_URL" > /workspaace/facefusion_url.txt
     echo "✅ FaceFusion Public URL: $GRADIO_URL"
 else
-    echo "Failed to get FaceFusion Gradio URL. Falling back to localhost." > facefusion_url.txt
+    echo "Failed to get FaceFusion Gradio URL. Falling back to localhost." > /workspace/facefusion_url.txt
 fi
 
 wait
