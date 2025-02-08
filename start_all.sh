5c7c2db08aa3e601dc653900de4df3a65f76bd4f
itsjet26 <127301212+itsjet26@users.noreply.github.com>
2025-02-08 17:09:03 +0800

    Update start_all.sh

--- start_all.sh
+++ start_all.sh
@@ -48,7 +48,7 @@ nohup python -u "/workspace/video-retalking/video_retalker_ui.py" > /workspace/v
 timeout=60
 while [[ $timeout -gt 0 ]]; do
     sleep 5
-    GRADIO_URL=$(grep -oP 'Running on (http|https)://[^\s]+' /workspace/video_retalker_ui.log | tail -1 | awk '{print $2}')
+    GRADIO_URL=$(grep -oP 'Running on public URL: \K(https://.*)' /workspace/facefusion.log | tail -1)
     
     if [[ -n "$GRADIO_URL" ]]; then
         echo "$GRADIO_URL" > /workspace/video_retalker_url.txt
