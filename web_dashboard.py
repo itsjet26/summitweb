10507d4c43010069ef467e324d9709fbd5043d25
itsjet26 <127301212+itsjet26@users.noreply.github.com>
2025-02-08 17:44:47 +0800

    Update web_dashboard.py

--- web_dashboard.py
+++ web_dashboard.py
@@ -6,7 +6,7 @@ def get_facefusion_url():
     return read_url_from_file("/workspace/facefusion_url.txt", "http://localhost:7860")
 
 def get_video_retalker_url():
-    return read_url_from_file("/workspaace/video_retalker_url.txt", "http://localhost:5001")
+    return read_url_from_file("/workspace/video_retalker_url.txt", "http://localhost:5001")
 
 def read_url_from_file(file_path, fallback_url):
     """ Read the URL from the file and return it, else return a fallback URL """
