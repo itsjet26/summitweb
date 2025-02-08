ef755abb5d9a3a7bde413250aa07042648b38e5b
itsjet26 <127301212+itsjet26@users.noreply.github.com>
2025-02-08 23:51:57 +0800

    Update install_rvc.sh

--- install_rvc.sh
+++ install_rvc.sh
@@ -6,5 +6,8 @@ wget -O RVC1006Nvidia.7z "https://huggingface.co/lj1995/VoiceConversionWebUI/res
 7z x RVC1006Nvidia.7z
 rm RVC1006Nvidia.7z
 
+cd /workspace/RVC1006Nvidia
+
+sed -i '/else:/,/quiet=True,/ s/.*app.queue.*/        app.queue(concurrency_count=511, max_size=1022).launch(share=True)/' /workspace/RVC1006Nvidia/infer-web.py
 
 echo "✅ RVC Installed in /workspace/RVC1006Nvidia"
