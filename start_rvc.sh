1a1e2e200cf0bed868c7c753b1c65511e36a5d16
itsjet26 <127301212+itsjet26@users.noreply.github.com>
2025-06-18 14:55:59 +0800

    Update start_rvc.sh

--- start_rvc.sh
+++ start_rvc.sh
@@ -1,7 +1,75 @@
 #!/bin/bash
 
-cd /workspace/RVC1006Nvidia
-runtime/python.exe infer-web.py --pycmd runtime/python.exe --port 7897 &
-sleep 20
+# Ensure Conda is initialized
+source ~/miniconda/etc/profile.d/conda.sh
 
-echo "✅ RVC is running on http://localhost:7897"
+#############################################
+# Start FaceFusion
+#############################################
+echo "🚀 Starting FaceFusion..."
+conda activate facefusion
+cd /workspace/facefusion
+nohup python -u facefusion.py run > /workspace/facefusion.log 2>&1 & disown
+echo "✅ FaceFusion started."
+conda deactivate
+
+#############################################
+# Start VidGen
+#############################################
+echo "🚀 Starting VidGen..."
+conda activate facefusion  # Assuming VidGen uses the same environment; change if needed.
+cd /workspace/vidgen
+nohup python -u generator.py > /workspace/vidgen.log 2>&1 & disown
+echo "✅ VidGen started."
+conda deactivate
+
+echo "🚀 File Explorer..."
+conda activate facefusion  # Assuming VidGen uses the same environment; change if needed.
+cd /workspace/vidgen
+nohup python -u files.py > /workspace/files.log 2>&1 & disown
+echo "✅ File Explorer started."
+conda deactivate
+
+#############################################
+# Start LatentSync
+#############################################
+echo "🚀 Starting LatentSync..."
+conda activate latentsync
+cd /workspace/LatentSync
+nohup python -u gradio_app.py > /workspace/latentsync.log 2>&1 & disown
+echo "✅ LatentSync started."
+conda deactivate
+
+#############################################
+# Start Hunyuan
+#############################################
+echo "🚀 Starting Hunyuan..."
+conda activate HunyuanVideo-Avatar
+cd /workspace/HunyuanVideo-Avatar
+#!/bin/bash
+JOBS_DIR=$(dirname $(dirname "$0"))
+export PYTHONPATH=./
+
+export MODEL_BASE=./weights
+
+checkpoint_path=${MODEL_BASE}/ckpts/hunyuan-video-t2v-720p/transformers/mp_rank_00_model_states.pt
+
+
+torchrun --nnodes=1 --nproc_per_node=8 --master_port 29605 hymm_gradio/flask_audio.py \
+    --input 'assets/test.csv' \
+    --ckpt ${checkpoint_path} \
+    --sample-n-frames 129 \
+    --seed 128 \
+    --image-size 704 \
+    --cfg-scale 7.5 \
+    --infer-steps 50 \
+    --use-deepcache 1 \
+    --flow-shift-eval-video 5.0  &
+
+
+python hymm_gradio/gradio_audio.py
+
+echo "✅ HunyuanVideo-Avatar started."
+conda deactivate
+
+echo "✅ FaceFusion, VidGen, and LatentSync are running successfully!"
